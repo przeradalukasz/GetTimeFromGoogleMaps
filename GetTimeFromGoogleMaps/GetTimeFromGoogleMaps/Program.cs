c002bad086f0141bfc43652c3a53e70a2b59ac3e
Łukasz Przerada <ronald-09l@wp.pl>
2017-05-08 09:04:54 +0200

    gmaps api added

---
+++ GetTimeFromGoogleMaps/GetTimeFromGoogleMaps/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Maps;
+using Google.Maps.Geocoding;
+
+namespace GetTimeFromGoogleMaps
+{
+    public class Program
+    {
+        public static string API_KEY = "AIzaSyD8hl5pO8Yd-yHJMefry2gi_FpN0t0ceFA";
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}
---
+++ GetTimeFromGoogleMaps/GetTimeFromGoogleMaps/Town.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Maps;
+using Google.Maps.Geocoding;
+
+namespace GetTimeFromGoogleMaps
+{
+    public class Town
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public FuzzyNumber FuzzyNumber { get; set; }
+
+        public Town(int id, double x, double y)
+        {
+            Id = id;
+            X = x;
+            Y = y;
+        }
+        
+        public Town()
+        { }
+        public void lol()
+        {
+            GoogleSigned.AssignAllServices(new GoogleSigned(API_KEY));
+            var request = new GeocodingRequest { Address = "1600 Amphitheatre Parkway", Sensor = false };
+            var response = GeocodingService.GetResponse(request);
+        }
+        l
+    }
+    
+}
+
